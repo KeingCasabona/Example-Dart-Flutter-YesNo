a7ad3d1c9fcc6f6a6171347f7697b6203b933234
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-25 14:28:39 -0500

    feat: Add loading message

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -49,6 +49,17 @@ class _ImageBuble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            //padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Center(
+              child: Text('Mi amor esta enviando una imagen...'),
+            ),
+          );
+        },
       ),
     );
   }
