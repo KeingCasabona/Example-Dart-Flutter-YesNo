7b85d5bfb1bc5e43317cc36cf4a9d06a5b7925ef
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-29 21:11:30 -0500

    feat: Add scroll

--- lib/presentation/providers/chat.provider.dart
+++ lib/presentation/providers/chat.provider.dart
@@ -2,18 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
-    Message(text: 'Ya regresaste del trabajo', fromWho: FromWho.me),
-    Message(text: 'Ya regresaste del trabajo', fromWho: FromWho.me),
-    Message(text: 'Ya regresaste del trabajo', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
     final newMessage = Message(text: text, fromWho: FromWho.me);
+    if (text.isEmpty) return;
     messageList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  //DAR UN EFECTO DE DELAY AL MOMENTO DE ENVIAR UN MENSAJE;
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
