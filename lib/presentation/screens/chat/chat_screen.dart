2de2bce1b85b6d8dba6e9dd0d75eb7179a55436e
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-29 20:08:22 -0500

    feat: Add messages to provider

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -51,7 +51,10 @@ class _ChatView extends StatelessWidget {
               ),
             ),
             //CAJA DE TEXTO:
-            const MessageFieldBox()
+            MessageFieldBox(
+              //onValue: (value) => chatProvider.sendMessage(value),
+              onValue: chatProvider.sendMessage,
+            )
           ],
         ),
       ),
