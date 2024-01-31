de52f0a7b37e52fea8e90a9932b782423d45bc95
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-30 19:48:44 -0500

    feat: Add response from YesNo

--- lib/presentation/providers/chat.provider.dart
+++ lib/presentation/providers/chat.provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  final ScrollController chatScrollController = ScrollController();
+  final chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
@@ -10,12 +12,19 @@ class ChatProvider extends ChangeNotifier {
   ];
 
   Future<void> sendMessage(String text) async {
-    final newMessage = Message(text: text, fromWho: FromWho.me);
     if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
+    if (text.endsWith('?')) {
+      herReply();
+    }
     notifyListeners();
     moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
   }
 
   //DAR UN EFECTO DE DELAY AL MOMENTO DE ENVIAR UN MENSAJE;
